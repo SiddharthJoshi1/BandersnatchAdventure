412e4f3d09a38bbcc61bb623b9920b35fe9f6572
SiddharthJoshi1 <siddharthjoshi1@hotmail.com>
2020-05-20 16:14:38 +0100

    initial commit

--- adventureGameV2/AdventureGame/src/type.fs
+++ adventureGameV2/AdventureGame/src/type.fs
@@ -1,1 +1,79 @@
 module type
+
+    type Inventory = {
+            attack: bool; 
+            defence: bool; 
+            health: int; 
+            key: int;
+     }
+
+    type health = private health of uint16
+
+    module health =
+     match num with
+     | var1 when var1 <= 1 -> None
+     | var2 when var2 >= 20 -> None
+     | _ -> num
+ 
+    type attackPower = private attackPower of uint16
+
+    module attackPower =
+     match num with
+     | var1 when var1 = 1 -> num 
+     | var2 when var2 = 3 -> num 
+     | _ -> None 
+ 
+    type range = private range of uint16
+
+    module range = 
+     match num with
+     | var1 when var1 = 1 -> num 
+     | var2 when var2 = 5 -> num 
+     | _ -> None 
+ 
+    type player = {
+         current_x: int;
+         current_y: int;
+         attackPower: int;
+         range: range;
+         attackState: bool;
+         attackUpActive: bool;
+         defenseUpActive: bool;
+    }
+
+    type tile = {
+        x: int;
+        y: int;
+    }
+
+    type Door = {
+        tile: tile;
+        open: bool;
+    }
+
+ 
+    type Stairs = {
+        tile:tile;
+    }
+ 
+    type Key = {
+        tile:tile;
+    }
+
+    type Item = {
+        type itemType = attackUp | defenseUp | healthUp;
+        tile:tile;
+    }
+
+    type tileType = 
+        | Empty
+        | Key
+        | Stairs
+        | Door
+        | Item 
+    
+    type gridcell = {
+        tile: tile
+        tileType: tileType
+    }
+    
