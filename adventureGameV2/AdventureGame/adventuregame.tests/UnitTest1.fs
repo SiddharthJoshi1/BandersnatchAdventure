6f5fe5182bcfb651a5e89a8089a81f6d32050f03
SiddharthJoshi1 <siddharthjoshi1@hotmail.com>
2020-06-05 23:52:21 +0100

    setup testing

---
+++ adventureGameV2/AdventureGame/adventuregame.tests/UnitTest1.fs
@@ -0,0 +1,15 @@
+module adventuregame.tests
+
+    open System
+    open NUnit.Framework
+    open Main
+
+        [<SetUp>]
+        let Setup () =
+            ()
+
+        [<Test>]
+            let Test1 () =
+                Assert.Pass() 
+        
+         
