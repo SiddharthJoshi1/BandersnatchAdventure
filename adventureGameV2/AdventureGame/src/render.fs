96feebda22449990d4f335fc2fc054867fc035f0
natasjar <60739798+natasjar@users.noreply.github.com>
2020-06-05 15:09:56 +0100

    render commented

--- adventureGameV2/AdventureGame/src/render.fs
+++ adventureGameV2/AdventureGame/src/render.fs
@@ -1,5 +1,4 @@
 module Render 
-    open Fable.Core
     open Fable.Core.JsInterop
     open Browser.Types
     open Browser
@@ -14,8 +13,9 @@ module Render
     // Get the contexts
     let ctx = myCanvas.getContext_2d()
     
-
+    //Square Size is the pixel width of each "tile"
     let squareSize = Type.squaresize
+    //Steps is how many "tiles" wide the grid is
     let steps = 20
 
     // gridWidth needs a float wo we cast our int operation to a float using the float keyword
@@ -29,21 +29,25 @@ module Render
     myCanvas.height <- gridWidth
     ctx.font <- "15px Comic Sans MS"
 
+    //sets the position and size of each html image
     let position (x:float,y:float) (img : HTMLImageElement) =
         img?style?left <- (x-5.).ToString() + "px"
         img?style?top <-  (y-5.).ToString() + "px"
         img?style?height <- squareSize.ToString() + "px"
     
+    //sets the position and size of the background image
     let bgStyle (img : HTMLImageElement) =
         img?style?left <- "9px"
         img?style?top <-  "9px"
         img?style?height <- gridWidth.ToString() + "px"
 
+    //casts the correct image path to the html image element 
     let image ((src : string), (id : string)) =
         let image = document.getElementById(id) :?> HTMLImageElement
         if image.src.IndexOf(src) = -1 then image.src <- src
         image
 
+    //called each time the update function runs
     let render  (dragon: Type.MovableDragon) 
                 (enemyObj:Type.Enemy) 
                 (itemList:Type.FilledTile List) 
@@ -61,14 +65,12 @@ module Render
         let lst = ["dfPotion"; "atkPotion"; "hpPotion"; "key"; "door0";"door1"]
         for i in lst do ("/img/whiteTile.png", i) |> image |> position (0.,0.)
 
+        //Background image
         (("/img/room" + level.ToString() + "bg.png"), "bg")
         |> image
         |> bgStyle
 
-
-        // ctx.fillStyle <- !^"#eddfb9" //beige
-        // ctx.fillRect(0.0,0.0,gridWidth,gridWidth)
-
+        // CANVAS GRID (now unused)
         // [0..steps] // this is a list
         //     |> Seq.iter( fun x -> // we iter through the list using an anonymous function
         //         let v = float ((x) * squareSize) 
@@ -79,15 +81,17 @@ module Render
         //     ) 
         // ctx.strokeStyle <- !^"#ddd" //light grey
 
+        //Player sprite
         (("/img/" + dragon.Direction + ".gif"),"player")
         |> image 
         |> position ( float(squareSize/2 - 1 + dragon.X), float(squareSize/2 - 1 + dragon.Y))
         
-       
+        //Enemy sprite
         (("/img/knight" + enemyObj.Dir + ".gif"),"enemy")
         |> image 
         |> position (float(squareSize/2 - 1 + enemyObj.X), float(squareSize/2 - 1 + enemyObj.Y))
         
+        //Item sprites (or removing item sprite when picked up)
         for i in itemList do
             let imgSrc = 
                 match i.Status with
@@ -98,15 +102,20 @@ module Render
                 |_ -> ("/img/whiteTile.png", "blank")
             imgSrc |> image |> position (float(squareSize/2 - 1 + i.X), float(squareSize/2 - 1 + i.Y))
 
+        //For checking walls/hazards/stairs match background image walls/hazards/stairs
         // for j in hazardList do
         //     ctx.fillStyle <- !^"#0000FF" //blue
         //     ctx.fillRect(float(j.X), float(j.Y),float(20),float(20))
 
         // for k in wallList do
-        //     ctx.fillStyle <- !^"#080808" //fucked up black
+        //     ctx.fillStyle <- !^"#000" 
         //     ctx.fillRect(float(k.X), float(k.Y),float(20),float(20))
-        // ctx.stroke() 
-            
+
+        // for m in stairList do
+        //     ctx.fillStyle <- !^"#03fc03"
+        //     ctx.fillRect(float(m.X), float(m.Y),float(squareSize),float(squareSize))
+
+        //Door images    
         let rec loop list (i:int) =
             match list with
             | head :: tail -> 
@@ -116,12 +125,9 @@ module Render
         loop doorList 0 
 
             
-        // for m in stairList do
-        //     ctx.fillStyle <- !^"#03fc03"
-        //     ctx.fillRect(float(m.X), float(m.Y),float(squareSize),float(squareSize))
-        //     ctx.fillStyle <- !^"#062829"
-
-        ctx.fillStyle <- !^"#000" //white text inv (contrast is still terrible?)
+        
+        //Writes the inventory on the side of the game
+        ctx.fillStyle <- !^"#000" //black
         let inventoryAttack :string =  if (inventory.AttackUpItem) then "Attack Up Powder: 1" else "Attack Up Powder: 0"
         let inventoryDefense :string =  if (inventory.DefenseUpItem) then "Defense Up Powder: 1" else "Defense Up Powder: 0"
         let inventoryHealth :string =  if (inventory.HealthUpItem) then "Health Up Powder: 1" else "Health Up Powder: 0"
@@ -131,10 +137,9 @@ module Render
             inventoryDefense; 
             inventoryHealth; 
             "Keys: " + string (inventory.Keys); 
-            //"Level: " + string level;
             "Attack Boosts Remaining: " + string dragon.AttackUp;
             "Defense Boosts Remaining: " + string dragon.DefenseUp]
-        
+        //iterated over the invList and places them 
         let rec loop (list:string list) acc =
             match list with
             | head :: tail -> 
@@ -143,7 +148,8 @@ module Render
             | [] -> acc |> ignore
         loop invList 20      
      
-        
+    //Game over Screen
+    //Blame Sid for the Comic Sans    
     let clearScreen (x:string) =
         ctx.font <- "40px Comic Sans MS"
         ctx.clearRect(0., 0., float(myCanvas.width), float(myCanvas.height))
